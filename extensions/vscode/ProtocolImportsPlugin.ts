cfbc61b6c1aedaa5f8e76b0b50df2fa2e67ff44f
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-12-10 14:45:22 +0530

    feat: src: resolve inbuilt module errors
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- extensions/vscode/ProtocolImportsPlugin.ts
+++ extensions/vscode/ProtocolImportsPlugin.ts
@@ -1,17 +1,17 @@
-import type { Rspack } from '@rsbuild/core';
+import type { Rspack } from "@rsbuild/core";
 
 export class ProtocolImportsPlugin {
   apply(compiler: Rspack.Compiler) {
     compiler.hooks.normalModuleFactory.tap(
-      'NormalModuleReplacementPlugin',
+      "NormalModuleReplacementPlugin",
       (nmf) => {
         nmf.hooks.beforeResolve.tap(
-          'NormalModuleReplacementPlugin',
+          "NormalModuleReplacementPlugin",
           (resource) => {
             // Remove the `node:` prefix
             // see: https://github.com/webpack/webpack/issues/14166
             if (/^node:/.test(resource.request)) {
-              resource.request = resource.request.replace(/^node:/, '');
+              resource.request = resource.request.replace(/^node:/, "");
             }
           },
         );
